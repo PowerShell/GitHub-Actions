0a6a6892e61e9ee99e03bf5b843bc9b3c20bc710
Travis Plunk <travis@ez13.net>
2020-07-27 14:45:19 -0700

    fix issues

--- PesterActionV1/build.ps1
+++ PesterActionV1/build.ps1
@@ -1,8 +1,29 @@
+[CmdletBinding(DefaultParameterSetName='build')]
+param(
+    [parameter(ParameterSetName='bootstrap',Mandatory)]
+    [switch]
+    $Bootstrap,
+    [parameter(ParameterSetName='build')]
+    [switch]
+    $Build
 
-Push-Location -Path $PSScriptRoot
-try {
-    ncc build .\invoke-pwsh.js -o _init
-}
-finally {
-    Pop-Location
+)
+
+switch ($PSCmdlet.ParameterSetName) {
+    'build' {
+        Push-Location -Path $PSScriptRoot
+        try {
+            $scripPath = (Resolve-Path .\invoke-pwsh.js).ProviderPath
+            ncc build $scripPath -o _init
+        }
+        finally {
+            Pop-Location
+        }
+    }
+    'bootstrap' {
+        npm i -g @zeit/ncc
+    }
+    default {
+        throw "Unknow parameterset $($pscmdlet.ParameterSetName)"
+    }
 }
