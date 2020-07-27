95a62a119dc2f625cd86590d45a4500a7db4601b
Travis Plunk <travis@ez13.net>
2020-07-27 15:25:17 -0700

    use start-nativeexecution

--- PesterActionV1/build.ps1
+++ PesterActionV1/build.ps1
@@ -12,6 +12,53 @@ param(
 
 )
 
+# this function wraps native command Execution
+# for more information, read https://mnaoumov.wordpress.com/2015/01/11/execution-of-external-commands-in-powershell-done-right/
+function script:Start-NativeExecution
+{
+    param(
+        [scriptblock]$ScriptBlock,
+        [switch]$IgnoreExitcode,
+        [switch]$VerboseOutputOnError
+    )
+    $backupEAP = $ErrorActionPreference
+    $ErrorActionPreference = "Continue"
+    try {
+        if($VerboseOutputOnError.IsPresent)
+        {
+            $output = & $ScriptBlock 2>&1
+        }
+        else
+        {
+            & $ScriptBlock
+        }
+
+        # note, if $ScriptBlock doesn't have a native invocation, $LASTEXITCODE will
+        # point to the obsolete value
+        if ($LASTEXITCODE -ne 0 -and -not $IgnoreExitcode) {
+            if($VerboseOutputOnError.IsPresent -and $output)
+            {
+                $output | Out-String | Write-Verbose -Verbose
+            }
+
+            # Get caller location for easier debugging
+            $caller = Get-PSCallStack -ErrorAction SilentlyContinue
+            if($caller)
+            {
+                $callerLocationParts = $caller[1].Location -split ":\s*line\s*"
+                $callerFile = $callerLocationParts[0]
+                $callerLine = $callerLocationParts[1]
+
+                $errorMessage = "Execution of {$ScriptBlock} by ${callerFile}: line $callerLine failed with exit code $LASTEXITCODE"
+                throw $errorMessage
+            }
+            throw "Execution of {$ScriptBlock} failed with exit code $LASTEXITCODE"
+        }
+    } finally {
+        $ErrorActionPreference = $backupEAP
+    }
+}
+
 switch ($PSCmdlet.ParameterSetName) {
     'build' {
         Push-Location -Path $PSScriptRoot
@@ -29,8 +76,8 @@ switch ($PSCmdlet.ParameterSetName) {
             $sudoCmd = 'sudo'
         }
 
-        $sb = [scriptblock]::Create("$sudoCmd npm i -g @zeit/ncc ")
-        Invoke-Expression -Command $sb
+        $ScriptBlock = [scriptblock]::Create("$sudoCmd npm i -g @zeit/ncc ")
+        Start-NativeExecution -ScriptBlock $ScriptBlock
     }
     default {
         throw "Unknow parameterset $($pscmdlet.ParameterSetName)"
