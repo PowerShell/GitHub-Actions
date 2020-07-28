48a4ed84c444ef287b32a2c3918e820d50f71b6e
James Truher <jimtru@microsoft.com>
2020-07-27 17:01:37 -0700

    harden logic for dealing with different versions of Pester

--- PesterActionV1/action.ps1
+++ PesterActionV1/action.ps1
@@ -15,17 +15,30 @@ if ($Version) {
 }
 
 Write-ActionInfo 'checking for Pester module...'
-$module = Get-Module -ListAvailable Pester
-if(!$module -and $module.Version -ne ${Version} ) {
-    Write-ActionInfo 'installing Pester module...'
+# find out if pester is installed and what the versions are
+# if the version is specified and doesn't exist, we'll need to install it
+# if the version is not specified and pester is installed we'll use what's installed
+$modules = Get-Module -ListAvailable Pester
+$requiredPester = $null
+if ( $Version ) {
+    $requiredPester = $modules | Where-Object {$_.Version -eq $Version}
+}
+else {
+    $requiredPester = $modules | Sort-Object Version | Select-Object -Last 1
+}
+
+if ( $requiredPester ) {
+    Write-ActionInfo ('Pester module version {0} already installed.' -f $requiredPester.Version)
+}
+else {
+    # if version is provided we'll get it, otherwise we get what we get
+    Write-ActionInfo 'installing Pester module {0}...' -f "version: ${version}"
     $ProgressPreference = 'SilentlyContinue'
     Install-Module @installModParams
 }
-else{
-    Write-ActionInfo 'Pester module already installed.'
-}
+
 # Don't rely on autoloading
-Import-Module Pester -Force -RequiredVersion ${Version}
+Import-Module @installModParams
 
 ## Pull in some inputs
 $script = Get-ActionInput script -Required
